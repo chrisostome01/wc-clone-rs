f1864e7c7d78f695e468f3f64f12faa087c26397
sezeranoJchrisostome <sezeranochrisostom123@gmail.com>
2025-12-24 13:24:26 +0200

    refactor: re-organise

--- src/main.rs
+++ src/main.rs
@@ -35,11 +35,11 @@ fn compute_stats(content: String) -> Stats {
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[args.len() - 1];
+    // ["/Users/chrysostome/Documents/personal/coding-challenges-rs/target/debug/ccwc", "ccwc", "./test.txt"]
+
+    // Reading a file
     let content = fs::read_to_string(file_path).expect("Unable to read the file.");
 
     let stats = compute_stats(content);
-    println!(
-        "{} {} {} {}",
-        stats.total_lines, stats.total_words, stats.total_bytes, stats.total_chars
-    );
+    println!("{} {} {} {}", stats.total_lines, stats.total_words, stats.total_bytes, stats.total_chars);
 }
